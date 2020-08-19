0c9885d2ee59a30439b45ef4e853c7c004b29204
Jelly-Fish-Dev <isaac.g.hawkins@gmail.com>
2020-08-19 15:42:38 +1000

    first commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::str::FromStr;
+use std::fs;
+
+
+fn main(){
+	let mut points: usize = 0;
+	let args: Vec<std::string::String> = std::env::args().collect();
+
+	points = usize::from_str(&args[1])
+		.expect("USEAGE: pi-to-point [Number] [pi.txt location]");
+
+	let filename = &args[2];
+
+	if points == 0{
+		std::process::exit(1);
+	}
+
+	let pi = fs::read_to_string(filename).expect("cannot read pi.txt");
+	println!("{}", &pi[0..points+2]);
+}
